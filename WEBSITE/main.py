9b22aa5d261809a1348994068f99fba3daee9f15
Blaze277 <aayanmaurya.tech5@gmail.com>
2026-01-25 13:36:07 +0530

    Update main.py

--- WEBSITE/main.py
+++ WEBSITE/main.py
@@ -166,7 +166,7 @@ def stocks():
             try:
                 
                 data = yf.download(ticker, period="1y", progress=False)
-                time.sleep(4.5)
+                time.sleep(7)
                 print(f"DEBUG: {ticker} - columns: {data.columns.tolist()}")
 
                 # Detect and flatten if MultiIndex (the real fix)
@@ -295,6 +295,7 @@ def stocks():
 if __name__  == "__main__":
 
     app.run(debug=True)
+
 
 
 
